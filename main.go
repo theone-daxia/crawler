0aba6906a68a75e797c4fc83c47d80fe666663bc
daliying <daliying>
2022-12-03 20:55:53 +0800

    print card title by regexp

--- main.go
+++ main.go
@@ -1,31 +1,57 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 )
 
+var titleRe = regexp.MustCompile(`<div class="small_toplink__GmZhY"[\s\S]*?<h2>([\s\S]*?)</h2>`)
+
 func main() {
-	url := "https://www.thepaper.cn/"
+	url := "https://www.thepaper.cn/channel_108856"
+	body, err := Fetch(url)
+	if err != nil {
+		fmt.Printf("read content failed:%v", err)
+		return
+	}
+
+	matches := titleRe.FindAllSubmatch(body, -1)
+	for _, m := range matches {
+		fmt.Println("fetch card news:", string(m[1]))
+	}
+}
+
+func Fetch(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Printf("fetch url error:%v\n", err)
-		return
+		panic(err)
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		fmt.Printf("error status code:%v\n", resp.StatusCode)
-		return
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DetermineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
 	if err != nil {
-		fmt.Printf("read content failed:%v\n", err)
-		return
+		fmt.Printf("fetch error%v\n", err)
+		return unicode.UTF8
 	}
-
-	fmt.Println("body:", string(body))
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
